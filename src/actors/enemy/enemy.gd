dfea569dd8a3dce65d353d05a089f65cf08c7290
Shawn Boutilier <shawn@lungdart.net>
2020-04-05 10:46:55 -0300

    Added enemy movement, and enemy squishing

--- src/actors/enemy/enemy.gd
+++ src/actors/enemy/enemy.gd
@@ -1,3 +1,85 @@
 extends Actor
 class_name Enemy
 
+export var dead_zone = 50
+export var sight_distance = 500
+export var walk_ramp_up_time = 0.2
+export var max_walk_speed = 100
+export var hp = 1
+
+var _walk_acceleration = 0.0
+var _facing_right = true
+var _player
+var _player_cast
+var _current_hp
+var _dead = false
+
+func _ready():
+	_walk_acceleration = max_walk_speed / walk_ramp_up_time
+	_player = $"../player"
+	_player_cast = $"player cast"
+	_current_hp = hp
+	
+	# Create a sight distance long cast, and just change the angle.
+	#  this prevents translating co-ordinates
+	_player_cast.set_cast_to( Vector2(sight_distance, 0) )
+
+
+func hit(damage):
+	_current_hp -= damage
+	if _current_hp < 0:
+		_current_hp = 0
+		die()
+
+
+func die():
+	queue_free()
+
+
+func _cast_to_player():
+	var direction = _player.global_position - _player_cast.global_position
+	_player_cast.set_cast_to(direction)
+	
+	return direction
+
+
+func _walk_right(dt):
+	_facing_right = true
+
+	if velocity.x >= 0:
+		velocity.x += _walk_acceleration * dt
+		velocity.x = min(velocity.x, max_walk_speed)
+	else:
+		velocity.x -= _walk_acceleration * dt
+		velocity.x = max(velocity.x, 0)
+
+
+func _walk_left(dt):
+	_facing_right = false
+
+	if velocity.x <= 0:
+		velocity.x += -_walk_acceleration * dt
+		velocity.x = max(velocity.x, -max_walk_speed)
+	else:
+		velocity.x -= -_walk_acceleration * dt
+		velocity.x = min(velocity.x, 0)
+
+
+func _idle(dt):
+	if velocity.x > 0:
+		velocity.x -= _walk_acceleration * dt
+		velocity.x = max(velocity.x, 0)
+	elif velocity.x < 0:
+		velocity.x += _walk_acceleration * dt
+		velocity.x = min(velocity.x, 0)
+
+
+func _physics_process(dt):
+	var direction = _cast_to_player()
+	if direction.length() < sight_distance and _player_cast.is_colliding():
+		if direction.x > 0:
+			_walk_right(dt)
+		elif direction.x < 0:
+			_walk_left(dt)
+	else:
+		_idle(dt)
--- src/actors/player/player.gd
+++ src/actors/player/player.gd
@@ -7,6 +7,8 @@ export var walk_ramp_up_time = 0.5
 export var walk_ramp_down_time = 0.2
 export var turn_ramp_down_time = 0.1
 export var jump_height = 250.0
+export var strength = 1
+export var hp = 3
 
 ### Derived parameters
 var _walk_acceleration = 0.0
@@ -14,11 +16,16 @@ var _walk_deceleration = 0.0
 var _turn_deceleration = 0.0
 var _jump_initialvelocity = 0.0
 
+var _animation_state
+var _sprite_sheet
+var _damage_cast
+
 var _idle_timer = 0.0
 var _facing_right = true
 var _jumping = false
 var _falling = true
-var _animation_state
+var _current_hp = 0
+
 
 func _ready():
 	### Setting these outside of ready scope causes the exported tuned values to not take effect
@@ -28,12 +35,21 @@ func _ready():
 	_jump_initialvelocity = sqrt(2 * gravity * jump_height)
 	
 	_animation_state = $"Animation States".get("parameters/playback")
+	_sprite_sheet = $"sprite sheet"
+	_damage_cast = $"DamageCast"
 	
+	_current_hp = hp
+	
+
+### Got hit by an enemy
+func hit(value):
+	print("Got damaged: ", value)
+
 
 ### Horizontal movement
 func walk_right(dt):
 	_facing_right = true
-	$"sprite sheet".flip_h = false
+	_sprite_sheet.flip_h = false
 	_idle_timer = 0.0
 
 	if not _jumping or not _falling:
@@ -49,7 +65,7 @@ func walk_right(dt):
 
 func _walk_left(dt):
 	_facing_right = false
-	$"sprite sheet".flip_h = true
+	_sprite_sheet.flip_h = true
 	_idle_timer = 0.0
 
 	if not _jumping or not _falling:
@@ -121,5 +137,11 @@ func _physics_process(dt):
 		_land(dt)
 	elif not _jumping and not is_on_floor():
 		_fall(dt)
+
+
+	# Handle squishing enemies
+	if _damage_cast.is_colliding():
+		var object = _damage_cast.get_collider()
+		object.hit(strength)
 	
 	
